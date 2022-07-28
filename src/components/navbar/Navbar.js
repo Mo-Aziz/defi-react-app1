f40242c134d45c6b67336d2230c196515bd1f3f0
Mo-Aziz <mohamed.aref32@gmail.com>
2022-07-28 01:07:58 -0500

    finished styling the navbar and the side toggle menu

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css';
-
+import{AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
 const Navbar = () => {
+    const [click,setClick] = useState(false)
+    const handleClick = () => setClick(!click);
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+        <div className='container'>
+            <h1>Defi</h1>
+            {/* nav-menu */}
+            <ul className={click ? "nav-menu active" : "nav-menu"}>
+                <li className='nav-item'>
+                    <a href='/'>Plateform</a>
+                </li>
+                <li className='nav-item'>
+                    <a href='/'>Developers</a>
+                </li>
+                <li className='nav-item'>
+                    <a href='/'>Community</a>
+                </li>
+                <li className='nav-item'>
+                    <a href='/'>About</a>
+                </li>
+                <li className='nav-item'>
+                 <button className='btn'>Use Defi</button>
+                </li>
+            </ul>
+
+            {/* side-toogle-icons */}
+            <div className='hamburger-icons' onClick={handleClick}>
+                {click ? <AiOutlineClose className='icon'/> : <AiOutlineMenu className='icon'/> }
+                
+            </div>
+        </div>
+    </div>
   )
 }
 
