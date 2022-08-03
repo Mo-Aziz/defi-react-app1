15bd9bb8cf17828ffd390395704182a543dd445b
Mo-Aziz <mohamed.aref32@gmail.com>
2022-08-02 19:26:03 -0500

    finished the developer container

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import About from "./components/about/About";
+import Developer from "./components/developer/Developer";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 
@@ -9,6 +10,7 @@ function App() {
        <Navbar/>
        <Hero/>
        <About/>
+       <Developer/>
     </>
   );
 }
